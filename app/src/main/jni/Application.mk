9ed43df6374d19a216425d56d052fb46c5f5d0a2
Abestanis <abestanis.gc@gmail.com>
2016-11-26 23:16:28 +0100

    Raise minimal Android version to 9
    The ndk dropped support for api level 8.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := all
-# The minimum API version we support is 8
-APP_PLATFORM := android-8
+# The minimum API version we support is 9
+APP_PLATFORM := android-9
 
 # Workaround for MIPS toolchain linker being unable to find liblog.so dependency
 APP_LDFLAGS := -llog
