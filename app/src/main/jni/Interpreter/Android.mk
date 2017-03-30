bae8b39c8439daa06c00aeeba23f00cecd53e623
Abestanis <abestanis.gc@gmail.com>
2017-03-30 22:19:06 +0200

    Fix library dependency

--- app/src/main/jni/Interpreter/Android.mk
+++ app/src/main/jni/Interpreter/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := pyInterpreter
 LOCAL_SRC_FILES := interpreter.c py_utils.c py_compatibility.c terminal.c
-LOCAL_SHARED_LIBRARIES := pyLog pythonPatch
+LOCAL_SHARED_LIBRARIES := pyLog
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../
 
 include $(BUILD_SHARED_LIBRARY)
