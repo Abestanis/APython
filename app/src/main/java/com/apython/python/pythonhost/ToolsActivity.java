7a40a9bc1576308154d2b4997e6994aba9b92b5b
Abestanis <abestanis.gc@gmail.com>
2017-10-20 20:34:28 +0200

    Add ToolsActivity

---
+++ app/src/main/java/com/apython/python/pythonhost/ToolsActivity.java
@@ -0,0 +1,26 @@
+package com.apython.python.pythonhost;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * This activity provides some helpful tools to the user.
+ * 
+ * Created by Sebastian on 21.11.2016.
+ */
+public class ToolsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tool_activity);
+    }
+    
+    public void onInstallPythonModule(View view) {
+        
+    }
+    
+    public void onRunPythonScript(View view) {
+        
+    }
+}
