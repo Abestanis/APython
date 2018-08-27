e0a7fb7f58a7726eac57195a0d9f50165165064a
Abestanis <abestanis.gc@gmail.com>
2018-08-27 23:07:27 +0200

    No need to support mips anymore since it was removed from the ndk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,9 +2,6 @@ APP_ABI := all
 # The minimum API version we support is 14
 APP_PLATFORM := android-14
 
-# Workaround for MIPS toolchain linker being unable to find liblog.so dependency
-APP_LDFLAGS := -llog
-
 #CLANG_SANTITIZE := address
 # This is necessary because the interpreter segfaults otherwise (possibly due to a c stack overflow)
 APP_CFLAGS := -O1
